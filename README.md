3c7c604a27c4a54f4d1b506371c56d9927431aa1
Xiang Chen <xchen@foqum.io>
2020-06-19 11:42:32 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# notify-idle
+
+## Usage:
+```
+# 1 recipient
+GMX_USER=sender@gmx.com GMX_PASSWORD=password notify-idle recipient@gmail.com MACHINEALIAS
+
+# 2 recipient
+GMX_USER=sender@gmx.com GMX_PASSWORD=password notify-idle recipient@gmail.com,recipient2@gmail.com MACHINEALIAS
+```
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+# python setup.py sdist
+# twine upload --skip-existing --repository pypi dist/*
+
+setuptools.setup(
+    name="notify-idle",
+    version="0.0.1",
+    author="Xiang Chen",
+    author_email="xiangchenchen96@gmail.com",
+    python_requires=">=3.6",
+    scripts=["notify-idle"],
+    install_requires=[
+        "psutil"
+    ],
+    description="Send email from GMX if machine is IDLE",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
